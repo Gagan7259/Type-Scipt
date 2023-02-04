a54469e471344d5ab0f20f545bacf2c9c294f84c
Gagan7259 <109654682+Gagan7259@users.noreply.github.com>
2023-02-04 14:01:39 +0530

    typescript

--- Day-1/myfunction.ts
+++ Day-1/myfunction.ts
@@ -1,21 +1,56 @@
-/* function  add(num:number){
+//when we put :  after the functions name and define the datatype which datatype u mention that data type values only accept
+//ex add(a:number):number its accepts only number its not accepts string boolean
+
+/* function  add(num:number):number{
     return num +2
 }
-add(20) */
+let abc=add(20) 
+console.log(abc);
+
 
 //string
-/* function getupper(val: string) {
+ function getupper(val: string) {
   return val.toLowerCase;
 }
-getupper("GAGAN"); */
+let ga=getupper("GAGAN"); 
+console.log(ga);
 
-/* //any
+
+//any
 function signuser(name: string, email: string, password: boolean) {}
-signuser(1, 2, 3); // its wrong
-signuser("gagan", "g@gmail", true); */
+//signuser(1, 2, 3); // its wrong
+signuser("gagan", "g@gmail", true); 
 
+let loginuser = (name: string, email: string, ispaid: boolean) => {
+  if (ispaid === void 1) {
+    ispaid = true;
+  }
+};
+loginuser("h", "g@gmail", true); */
 
-let loginuser=(name:string,email:string ,ispaid:boolean)=>{
-
+/* function getmyval(val: number) {
+  if (val > 5) {
+    return "200ok";
 }
-loginuser("h","g@gmail")
+}
+getmyval(100); */
+
+const getHello = (s: string): string => {
+  return "ok";
+};
+const heros = ["NTR", "NBK", "NKR"];
+//const heros=[1,2,3]
+//when mappinhg a function array value are numbers type script automatically convert function as perticular datatypes in array
+heros.map((hero): string => {
+  return "he is ${hero}";
+  //return 2
+});
+
+function consoleerror(errmsg: string): void {
+  console.log(errmsg);
+  return;
+}
+
+function handleerror(errmsg: string): void {
+  throw new Error(errmsg);
+}
