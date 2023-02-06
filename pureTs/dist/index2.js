6bc8ee48f4180b26f53b8df7357efeceb3546e45
Gagan7259 <109654682+Gagan7259@users.noreply.github.com>
2023-02-06 20:56:11 +0530

    typescript

--- pureTs/dist/index2.js
+++ pureTs/dist/index2.js
@@ -4,10 +4,24 @@ class User1 {
         this.email = email;
         this.name = name;
         this.UserId = UserId;
+        this._cousercont = 1;
+    }
+    deleteToken() {
+        console.log("token delwtwd");
     }
     get getAppleEmail() {
         return `apple $(this.eamil)`;
     }
+    get cousercont() {
+        return this._cousercont;
+    }
+    set cousercont(cnum) {
+        if (cnum <= 3) {
+            throw new Error("poyi saavu pora");
+        }
+        this._cousercont = cnum;
+    }
 }
 const gagan1 = new User1("g@gamil", "manoj", "23");
 gagan1.name;
+//gagan1.deleteToken();
--- pureTs/src/index2.ts
+++ pureTs/src/index2.ts
@@ -1,12 +1,26 @@
 class User1 {
+  private _cousercont = 1;
   constructor(
     public email: string,
     public name: string,
     private UserId: string
   ) {}
+  private deleteToken() {
+    console.log("token delwtwd");
+  }
   get getAppleEmail(): string {
     return `apple $(this.eamil)`;
+  }
+  get cousercont(): number {
+    return this._cousercont;
+  }
+  set cousercont(cnum) {
+    if (cnum <= 3) {
+      throw new Error("poyi saavu pora");
+    }
+    this._cousercont = cnum;
   }
 }
 const gagan1 = new User1("g@gamil", "manoj", "23");
 gagan1.name;
+//gagan1.deleteToken();
