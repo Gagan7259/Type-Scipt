e5cfabcec24bd228e5f1568267b605f3d7e674ee
Gagan7259 <109654682+Gagan7259@users.noreply.github.com>
2023-02-07 18:44:41 +0530

    typescript

---
+++ pureTs/dist/InstanceofNarrow.js
@@ -0,0 +1,9 @@
+"use strict";
+function Logvalue(x) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    }
+    else {
+        console.log(x.toUpperCase());
+    }
+}
---
+++ pureTs/dist/pridicateType.js
@@ -0,0 +1,14 @@
+"use strict";
+function isFish(pet) {
+    return pet.swim !== undefined;
+}
+function getFood(pet) {
+    if (isFish(pet)) {
+        pet;
+        return "fish food ";
+    }
+    else {
+        pet;
+        return "bird food";
+    }
+}
